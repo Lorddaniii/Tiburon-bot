cba459c3440e651bb91b6816465e7fbe887d09fd
Angel1511R <142075101+Angel1511R@users.noreply.github.com>
2023-10-14 22:38:39 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ const chalk = require('chalk')
 
 //---------[ PROPIETADO/OWNER ]---------
 global.owner = [
-["595975740803", "Owner", true], 
+["524434364440", "Owner", true], 
 ["593968585383"],
 ["5492266466080"], 
 ["595992302861"], 
